66667bce4ab7eba147299532c07054e6661768f2
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2024-01-06 22:22:55 +0700

    минимальная высота для пространства между header footer

--- src/pages/root/Root.tsx
+++ src/pages/root/Root.tsx
@@ -1,12 +1,16 @@
 import Footer from "@/modules/footer/Footer";
 import Header from "@/modules/header/Header";
+import st from "./Root.module.scss";
 import React from "react";
 import { Outlet } from "react-router-dom";
 const Root = () => {
   return (
     <>
       <Header />
-      <Outlet />
+      <div className={st.outlet}>
+        <Outlet />
+      </div>
+
       <Footer />
     </>
   );
